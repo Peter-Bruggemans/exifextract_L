76a3d2b803f4a952e0ac45c9c94fc10976d00476
Peter Bruggemans <Peter-Bruggemans@users.noreply.github.com>
2016-06-27 23:32:06 +0200

    Update exifextract.py

--- exifextract.py
+++ exifextract.py
@@ -4,11 +4,9 @@ import os
 from subprocess import check_output
 
 # variabelen voor de broninstructieregel
-broninstructie = "ls"
-bronoptie = ""
-brondrive = "/"
-bronpad = "/home/peter/Afbeeldingen"
-bronbestand = "*.svg"
+bronregel = """ls -R /home/peter/Fotos | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }'"""
+bronpad = '/home/peter/Fotos/'
+bronbestand = 'CR2'
 
 # variablelen voor de doelinstructieregel
 doelinstructie = "exiftool -T -canon -csv"
@@ -17,29 +15,28 @@ doeldrive = "/"
 doelpad = "/home/peter"
 doelbestand = "test.csv"
 
-# bouw de broninstructieregel
-bronregel = broninstructie + ' ' + brondrive + bronpad + '/' + bronbestand
-# pas de broninstructieregel aan aan het os(/ wordt \) en voeg de opties toe(/ moet blijven)
-osbronregel = (os.path.normpath(bronregel)) + " " + bronoptie
-
 # controle
 #print (bronregel)
-print (osbronregel)
 
 # voer de broninstructie uit
-result=check_output(osbronregel, shell=True)
+result=check_output(bronregel, shell=True)
 # verdeel het resultaat in regels
 lines = result.split('\n')
-# maak de doelinstructieregel aan per broninstructieregel
+# filter op bronpad en bronbestand en maak de doelinstructieregel aan per broninstructieregel
+teller = 0
+print('Writing')
 for line in lines:
     if len(line) > 0:
-        # bouw de doelinstructieregel
-        doelregel = doelinstructie + ' ' + line + ' >> ' + doeldrive + doelpad + '/' + doelbestand
-        # pas de broninstructieregel aan aan het os(/ wordt \) en voeg de opties toe(/ moet blijven)
-        osdoelregel = (os.path.normpath(doelregel)) + " " + doeloptie
-        # laat de doelinstructieregel zien
-        print (osdoelregel)
-        # voer de doelinstructieregel uit
-        result2 = check_output(osdoelregel, shell=True)
-    
-  
+        if line[-3:] == bronbestand:
+            if line[0:len(bronpad)] == bronpad:
+                teller = teller + 1
+                # bouw de doelinstructieregel
+                doelregel = doelinstructie + ' ' + line + ' >> ' + doeldrive + doelpad + '/' + doelbestand
+                # pas de broninstructieregel aan aan het os(/ wordt \) en voeg de opties toe(/ moet blijven)
+                osdoelregel = (os.path.normpath(doelregel)) + " " + doeloptie
+                # laat de doelinstructieregel zien
+                print str(teller) + " row(s) written"
+                # voer de doelinstructieregel uit
+                result2 = check_output(osdoelregel, shell=True)
+                
+print('Finished')
